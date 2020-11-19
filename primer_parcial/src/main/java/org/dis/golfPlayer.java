872c32453861cc1bfe19240bed3054c0f4f8ddd9
Andres <andres.arcones.crespo@gmail.com>
2020-11-19 20:29:54 +0100

    Creado el proyecto maven y la clase jugador

---
+++ primer_parcial/src/main/java/org/dis/golfPlayer.java
@@ -0,0 +1,13 @@
+//Clase para crear el objeto plano (POJO) con anotaciones
+package org.dis;
+
+public class Jugador {
+    private String posicion;
+    private String nombre;
+    private String apellido;
+    private String pais;
+    private String edad;
+
+
+
+}
